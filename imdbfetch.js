553b33afc550bca8eee333a3bc597d3676c839ac
moeenz <moeenzdev@gmail.com>
2015-06-07 18:55:33 +0430

    added some features

--- imdbfetch.js
+++ imdbfetch.js
@@ -10,7 +10,7 @@ else {
     console.error("No movie link provided");
     return;
 }
-//var url = process.argv[2]; 
+//var url = process.argv[2];
 
 var photos_postfix = "mediaindex"; // Link postfix for movie photos.
 
@@ -40,7 +40,7 @@ function fetch(url, callback) {
 			});
 			res.on("end", function () {
 				callback(data); // Calls the specified callback function when all datas are fetched.
-			});				
+			});
 		})
 		.on("error", function () { // In case of failure to load the page.
 			callback(null);
@@ -48,27 +48,57 @@ function fetch(url, callback) {
 };
 
 /* Calling our function to process the movie page. */
+
 fetch(url, function (data) {
 	if(data) { // If there is any data we proceed.
 		var $ = cheerio.load(data);   // Initializing cheerio.
 		movie.name = $("span.itemprop[itemprop='name']").html();    // Movie name.
-		movie.year = $("h1.header span.nobr a").text(); // Movie release date.
-		movie.rating = $("div.star-box.giga-star div.titlePageSprite.star-box-giga-star").text();   // Movie rating.
-		movie.desc = $("p[itemprop='description']").text(); // Movie description.
-        
-		fetch(url + photos_postfix, function (photos) {   // Now we bring first page of photos of the movie
+        movie.date = $($("meta[itemprop='datePublished']")[0]).attr("content"); // Movie release date.
+        movie.contentrating = $($("meta[itemprop='contentRating']")[0]).attr("content");
+		movie.rating = $("div.star-box.giga-star div.titlePageSprite.star-box-giga-star").text().trim();   // Movie rating.
+		movie.description = $("p[itemprop='description']").text(); // Movie description.
+        movie.genre = "" + $($("span[itemprop='genre']")[0]).text() + ", " + $($("span[itemprop='genre']")[1]).text();
+        movie.poster = $($("img[itemprop='image']")[0]).attr("src");
+
+        movie.roles = [];
+        movie.roles.push({
+            name: $($("div[itemprop='director'] a span")[0]).text(),
+            role: "director"
+        });
+        movie.roles.push({
+            name: $($("div[itemprop='creator'] a span")[0]).html(),
+            role: "writer"
+        });
+
+
+        var cast_list = $("div#titleCast table tr");
+
+        for (var j = 1; j < 8; j++) {
+            name = $(cast_list[j]).find("td span.itemprop[itemprop='name']").text();
+
+            cast_role = $(cast_list[j]).find("td.character div a");
+            role = "" + $(cast_role[0]).text() + ($(cast_role[1]).length > 0 ? " / " + $(cast_role[1]).text() : "");
+
+            movie.roles.push({
+                role: role,
+                name: name
+            });
+        }
+
+        fetch(url + photos_postfix, function (photos) {   // Now we bring first page of photos of the movie
 			if(photos) {
                 movie.photos = [];
 				var $_I = cheerio.load(photos); // Initializing cheerio for the photos page.
 				var photos = $_I("div#main div#media_index_thumbnail_grid a img");
-                /* Processing 5 first photos of the movie. */
+                // Processing 5 first photos of the movie.
 				for(var i = 0; i < 5; i++) {
                     movie.photos.push({
                         photo_alt: $(photos[i]).attr("alt"),  // Photo description.
+                        photo_thumb: $(photos[i]).attr("src"), // Photo thumbnail.
                         photo_link: link_modifier($(photos[i]).attr("src"))  // Photo full size link.
                     });
 				}
-                
+
                 console.log(movie); // Print out the result.
 			}
 			else {
